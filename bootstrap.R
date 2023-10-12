99bddcec1a4de8ebf38d4accfe66bade8e2af77b
Lorival-Jr <lorivaljunior0123@gmail.com>
2023-10-12 13:59:10 -0300

    Análise dos Intervalos de confiança e das medidas diagnósticas

--- bootstrap.R
+++ bootstrap.R
@@ -68,9 +68,9 @@ Bootstrap <- function(N,enes,var,banco= abalone, conf.level=0.95, seed=NULL)
     estatistiscas <- lapply(split(amostras[[i/enes[1]]], ceiling(seq_along(amostras[[i/enes[1]]])/i)), 
                             FUN = function(x)
                             {return(c(mean(x), median(x)))})
-    
+
     estatistiscas <- do.call(rbind, estatistiscas)
-    
+
     valores <-  c(mean(estatistiscas[,1]), median(estatistiscas[,2]), # media e mediana
                   sd(estatistiscas[,1]), # Erro media
                   sd(estatistiscas[,2]), # Erro mediana
@@ -93,10 +93,11 @@ Bootstrap <- function(N,enes,var,banco= abalone, conf.level=0.95, seed=NULL)
         IC_norm_median <- c(valores[2] - qnorm(conf)*valores[4], valores[2] + qnorm(conf)*valores[4])
         
         # Percentil Reverso (básico)
-        Q <- quantile(estatistiscas, c(1-conf, conf))
+        Q1 <- quantile(estatistiscas[,1], c(1-conf, conf))
+        Q2 <- quantile(estatistiscas[,2], c(1-conf, conf))
         
-        IC_perc_mean <-   c(2*valores[1] - Q[[1]], 2*valores[1] - Q[[2]])
-        IC_perc_median <- c(2*valores[2] - Q[[1]], 2*valores[2] - Q[[2]])
+        IC_perc_mean <-   c(2*valores[1] - Q1[[2]], 2*valores[1] - Q1[[1]])
+        IC_perc_median <- c(2*valores[2] - Q2[[2]], 2*valores[2] - Q2[[1]])
         
         # t-Student
         
@@ -158,10 +159,172 @@ Bootstrap <- function(N,enes,var,banco= abalone, conf.level=0.95, seed=NULL)
   
 }
 
-dados <- Bootstrap(100, seq(20,200,20), 'diameter', conf.level =  c(0.9, 0.95, 0.99))
-dados
 
-# Intervalo de confiança --------------------------------------------------
+# diametrer ----------------------------------------------------------------
 
-# Determinar os intervalos de 90%, 95% e 99% via bootstrap usando os tipos "normal", "percentil", "t de Student" e "BCA". Todos devem ser implementados na mão, sem uso de pacotes.
+diameter <- Bootstrap(10000, seq(20,200,20), 'diameter', conf.level =  c(0.9, 0.95, 0.99))
+
+
+as.numeric(diameter$IC_media$LI[1:40]) < mean(abalone$diameter) & mean(abalone$diameter) < as.numeric(diameter$IC_media$LS[1:40])
+# OU seja com 0.9 de confiança todos intervalos contém o parametro verdadeiro da media
+
+as.numeric(diameter$IC_media$LI[41:80]) < mean(abalone$diameter) & mean(abalone$diameter) < as.numeric(diameter$IC_media$LS[41:80])
+# OU seja com 0.95 de confiança todos intervalos contém o parametro verdadeiro da media
+
+as.numeric(diameter$IC_media$LI[81:120]) < mean(abalone$diameter) & mean(abalone$diameter) < as.numeric(diameter$IC_media$LS[81:120])
+# OU seja com 0.99 de confiança todos intervalos contém o parametro verdadeiro da media
+
+as.numeric(diameter$IC_mediana$LI[1:40]) < median(abalone$diameter) &
+  mean(abalone$diameter) < as.numeric(diameter$IC_mediana$LS[1:40])
+# OU seja com 0.9 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+as.numeric(diameter$IC_mediana$LI[41:80]) < median(abalone$diameter) &
+  mean(abalone$diameter) < as.numeric(diameter$IC_mediana$LS[41:80])
+# OU seja com 0.95 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+as.numeric(diameter$IC_mediana$LI[81:120]) < median(abalone$diameter) &
+  mean(abalone$diameter) < as.numeric(diameter$IC_mediana$LS[81:120])
+# OU seja com 0.99 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+
+# Whole.weight ------------------------------------------------------------
+
+
+
+whole.weight <- Bootstrap(10000, seq(20,200,20), 'whole_weight', conf.level =  c(0.9, 0.95, 0.99))
+
+
+as.numeric(whole.weight$IC_media$LI[1:40]) < mean(abalone$whole_weight) & mean(abalone$whole_weight) < as.numeric(whole.weight$IC_media$LS[1:40])
+# OU seja com 0.9 de confiança todos intervalos contém o parametro verdadeiro da media
+
+as.numeric(whole.weight$IC_media$LI[41:80]) < mean(abalone$whole_weight) & mean(abalone$whole_weight) < as.numeric(whole.weight$IC_media$LS[41:80])
+# OU seja com 0.95 de confiança todos intervalos contém o parametro verdadeiro da media
+
+as.numeric(whole.weight$IC_media$LI[81:120]) < mean(abalone$whole_weight) & mean(abalone$whole_weight) < as.numeric(whole.weight$IC_media$LS[81:120])
+# OU seja com 0.99 de confiança todos intervalos contém o parametro verdadeiro da media
+
+
+
+as.numeric(whole.weight$IC_mediana$LI[1:40]) < median(abalone$whole_weight) &
+  mean(abalone$whole_weight) < as.numeric(whole.weight$IC_mediana$LS[1:40])
+# OU seja com 0.9 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+as.numeric(whole.weight$IC_mediana$LI[41:80]) < median(abalone$whole_weight) &
+  mean(abalone$whole_weight) < as.numeric(whole.weight$IC_mediana$LS[41:80])
+# OU seja com 0.95 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+as.numeric(whole.weight$IC_mediana$LI[81:120]) < median(abalone$whole_weight) &
+  mean(abalone$whole_weight) < as.numeric(whole.weight$IC_mediana$LS[81:120])
+# OU seja com 0.99 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+
+# lenght ------------------------------------------------------------------
+
+length <- Bootstrap(10000, seq(20,200,20), 'length', conf.level =  c(0.9, 0.95, 0.99))
+
+
+as.numeric(length$IC_media$LI[1:40]) < mean(abalone$length) &
+  mean(abalone$length) < as.numeric(length$IC_media$LS[1:40])
+# OU seja com 0.9 de confiança todos intervalos contém o parametro verdadeiro da media
+
+as.numeric(length$IC_media$LI[41:80]) < mean(abalone$length) &
+  mean(abalone$length) < as.numeric(length$IC_media$LS[41:80])
+# OU seja com 0.95 de confiança todos intervalos contém o parametro verdadeiro da media
+
+as.numeric(length$IC_media$LI[81:120]) < mean(abalone$length) &
+  mean(abalone$length) < as.numeric(length$IC_media$LS[81:120])
+# OU seja com 0.99 de confiança todos intervalos contém o parametro verdadeiro da media
+
+as.numeric(length$IC_mediana$LI[1:40]) < median(abalone$length) &
+  mean(abalone$length) < as.numeric(length$IC_mediana$LS[1:40])
+# OU seja com 0.9 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+as.numeric(length$IC_mediana$LI[41:80]) < median(abalone$length) &
+  mean(abalone$length) < as.numeric(length$IC_mediana$LS[41:80])
+# OU seja com 0.95 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+as.numeric(length$IC_mediana$LI[81:120]) < median(abalone$length) &
+  mean(abalone$length) < as.numeric(length$IC_mediana$LS[81:120])
+# OU seja com 0.99 de confiança todos intervalos contém o parametro verdadeiro da mediana
+
+
+# Análise das medidas Dianóstico ------------------------------------------
+
+diameter$estimativas
+whole.weight$estimativas
+length$estimativas
+
+grafico_diag <- function(var, ylab, ylim=NULL, linha_0 = TRUE, arquivo = 'lixo')
+{
+  
+  medida <- sub('.*_', '',var)
+  
+  
+  jpeg(paste0('img/', arquivo, '.jpg'), 1080, 720, quality = 100,)
+  
+  if(!is.null(ylim)){
+    plot(x= whole.weight$estimativas$n, whole.weight$estimativas[[var]],
+         type = 'b', col = '#004586', lty = 2, lwd = 2, ylim = ylim,
+         main = paste(ylab, 'Bootstrap para',medida, 'por tamanho de amostra'),
+         xlab = 'Tamanho de amostra',
+         ylab = ylab)}
+  else{
+    plot(x= whole.weight$estimativas$n, whole.weight$estimativas[[var]],
+         type = 'b', col = '#004586', lty = 2, lwd = 2,
+         main = paste(ylab, 'Bootstrap para',medida, 'por tamanho de amostra'),
+         xlab = 'Tamanho de amostra',
+         ylab = ylab)}
+  
+    lines(x= diameter$estimativas$n,y=diameter$estimativas[[var]], type = 'b', col = '#FFD320', lty = 2, lwd = 2)
+    lines(x= length$estimativas$n,y=length$estimativas[[var]],     type = 'b', col = '#579D1C', lty = 2, lwd = 2)
+    if(linha_0 == TRUE) abline(h = 0, lty = 2, lwd = 2, col = 'red')
+    legend(x = "topright",          # Position
+           legend = c('Whole.Weight',
+                      'Length',
+                      'Diameter'),  
+           lty = 2,           # Line types
+           col = c('#004586', '#579D1C', '#FFD320'),           # Line colors
+           lwd = 2)  
+  
+
+    dev.off()
+    if(!is.null(ylim)){
+      plot(x= whole.weight$estimativas$n, whole.weight$estimativas[[var]],
+           type = 'b', col = '#004586', lty = 2, lwd = 2, ylim = ylim,
+           main = paste(ylab, 'Bootstrap para',medida, 'por tamanho de amostra'),
+           xlab = 'Tamanho de amostra',
+           ylab = ylab)}
+    else{
+      plot(x= whole.weight$estimativas$n, whole.weight$estimativas[[var]],
+           type = 'b', col = '#004586', lty = 2, lwd = 2,
+           main = paste(ylab, 'Bootstrap para',medida, 'por tamanho de amostra'),
+           xlab = 'Tamanho de amostra',
+           ylab = ylab)}
+    
+    lines(x= diameter$estimativas$n,y=diameter$estimativas[[var]], type = 'b', col = '#FFD320', lty = 2, lwd = 2)
+    lines(x= length$estimativas$n,y=length$estimativas[[var]],     type = 'b', col = '#579D1C', lty = 2, lwd = 2)
+    if(linha_0 == TRUE) abline(h = 0, lty = 2, lwd = 2, col = 'red')
+    legend(x = "topright",          # Position
+           legend = c('Whole.Weight',
+                      'Length',
+                      'Diameter'),  
+           lty = 2,           # Line types
+           col = c('#004586', '#579D1C', '#FFD320'),           # Line colors
+           lwd = 2)  
+}
+
+whole.weight$estimativas
+grafico_diag(var = 'vicio_media',ylab = 'Vício', arquivo = 'vicio_media')
+# O vício para media é baixo, e conforme o n aumenta ele se aproxima de zero
+grafico_diag(var = 'vicio_mediana',ylab = 'Vício', ylim = c(-0.0025, 0.0008), arquivo = 'vicio_mediana')
+# O vício para mediana é baixo, e conforme o n aumenta ele se aproxima de zero
+
+grafico_diag(var = 'erro_media',ylab = 'Erro', arquivo = 'erro_media', ylim = c(0,0.11))
+# Erro para media é baixo, e conforme o n aumenta ele diminui
+grafico_diag(var = 'erro_mediana',ylab = 'Erro', ylim = c(0,0.15), arquivo = 'erro_mediana')
+# Erro para mediana é baixo, e conforme o n aumenta ele diminui
+grafico_diag(var = 'EQM_media',ylab = 'EQM', arquivo = 'EQM_media')
+# O EQM da media é minusculo, aproximadamente 0.0000006 para whole.weight em n = 20, conforme n aumenta ele estabiliza em 0
+grafico_diag(var = 'EQM_mediana',ylab = 'EQM', ylim = c(0, 8e-6), arquivo = 'EQM_mediana')
+# O EQM da mediana é minusculo, aproximadamente 0.000006, mesmo para n =20, conforme n aumenta ele estabiliza em 0
 
